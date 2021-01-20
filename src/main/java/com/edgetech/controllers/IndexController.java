e98342d5b139a1b57dcc7bb4be4f38eadb829639
dbacy <69469697+dbacy@users.noreply.github.com>
2021-01-20 13:01:57 -0600

    Update

--- src/main/java/com/edgetech/controllers/IndexController.java
+++ src/main/java/com/edgetech/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.edgetech.controllers;
 
 import com.edgetech.models.City;
 //import com.edgetech.models.Country;
+import com.edgetech.models.Country;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,8 @@ public class IndexController {
     Connection con;
 
     public IndexController() {
-        String url = "jdbc:mysql://localhost:3306/world?useSSL=false"; //	user=root;password=password";
-        //  String url = "jdbc:sqlserver://localhost:1433;databaseName=world;";
+//        String url = "jdbc:mysql://localhost:3306/world?useSSL=false"; //	user=root;password=password";
+          String url = "jdbc:sqlserver://localhost:1433;databaseName=world;";
 
         try {
             con = DriverManager.getConnection(url, "root", "password");
@@ -45,8 +46,8 @@ public class IndexController {
             ResultSet results = stmt.executeQuery(query);
             //  retrieve all rows from the result set
             while (results.next()) {
-                population += results.getInt(3);        //  sum up the population for the cities in the result set
-                City city = new City(results.getString("name"), results.getString("district"), results.getInt(3));
+                population += results.getInt("population");        //  sum up the population for the cities in the result set
+                City city = new City(results.getString("name"), results.getString("district"), results.getInt("population"));
                 cities.add(city);                       //  this will be our list of cities to save to our web page
             }
         } catch (Exception except) {
@@ -58,5 +59,42 @@ public class IndexController {
         model.addAttribute("population", population);
         model.addAttribute("district", district);
         return "cities";
+    }
+
+    @RequestMapping("continents")
+    public String aaa(@RequestParam String continent, Model model) throws Exception {
+
+        List<Country> countries = new ArrayList<>();
+        int population = 0;
+
+        try {
+            Statement stmt = con.createStatement();
+            String query = "SELECT Name, Continent, HeadofState, Capital, LifeExpectancy, Population FROM Country ";
+            if (continent != null && continent.length() > 0)
+                query += "WHERE Continent like '%" + continent + "%' ";
+            query += "ORDER BY Name";
+
+            ResultSet results = stmt.executeQuery(query);
+            //  retrieve all rows from the result set
+            while (results.next()) {
+                population += results.getInt("population");
+                Country country = new Country(results.getString("name"), results.getString("continent"),
+                        results.getString("headOfState"), results.getString("capital"),
+                        results.getInt("lifeExpectancy"),  results.getInt("population"));
+                countries.add(country);
+            }
+        } catch (Exception except) {
+            System.out.println(except.getMessage());
+            SQLException ex = new SQLException("Query or Connection Failed: " + except.getMessage());
+        }
+        model.addAttribute("title", "First JDBC Application");
+        model.addAttribute("countries", countries);
+        model.addAttribute("continents",continent);
+//        model.addAttribute("headOfStates", headOfState);
+//        model.addAttribute("capitals",capitals);
+//        model.addAttribute("lifeExpectancy", lifeExpectancy);
+        model.addAttribute("population", population);
+
+        return "countries";
     }
 }
---
+++ src/main/java/com/edgetech/models/Country.java
@@ -0,0 +1,55 @@
+package com.edgetech.models;
+
+public class Country {
+    String name;
+    String continent;
+    String headOfState;
+    String capital;
+    int lifeExpectancy;
+    int population;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getContinent() { return continent; }
+
+    public void setContinent(String continent) { this.continent = continent; }
+
+    public String getHeadOfState() { return headOfState; }
+
+    public void setHeadOfState(String headOfState) { this.headOfState = headOfState; }
+
+    public String getCapital() { return capital; }
+
+    public void setCapital(String capital) { this.capital = capital; }
+
+    public int getLifeExpectancy() { return lifeExpectancy; }
+
+    public void setLifeExpectancy(int lifeExpectancy) { this.lifeExpectancy = lifeExpectancy; }
+
+    public int getPopulation() { return population; }
+
+    public void setPopulation(int population) { this.population = population; }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", headOfState='" + headOfState + '\'' +
+                ", capital='" + capital + '\'' +
+                ", lifeExpectancy=" + lifeExpectancy +
+                ", population=" + population +
+                '}';
+    }
+
+    public Country(String name, String continent, String headOfState, String capital, int lifeExpectancy, int population) {
+        this.name = name;
+        this.continent = continent;
+        this.headOfState = headOfState;
+        this.capital = capital;
+        this.lifeExpectancy = lifeExpectancy;
+        this.population = population;
+    }
+}
